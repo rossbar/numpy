838b45b26f9e3524977d008fde975d7ea8ad6418
Ross Barnowski <rossbar@berkeley.edu>
2020-04-25 17:22:45 -0700

    Update documentation

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.19.0.dev0+c0fe893',
+    VERSION: '1.19.0.dev0+b751304',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
